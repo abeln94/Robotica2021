b4fe6379b7a1b7b088d8c4d4479973b2bb832adf
tisho <albertisho99@gmail.com>
2021-03-18 19:20:21 +0100

    Pruebas de foto

--- prueba.py
+++ prueba.py
@@ -20,27 +20,9 @@ rawCapture = PiRGBArray(cam, size=(320, 240))
 # allow the camera to warmup
 time.sleep(0.1)
 
+cam.capture(rawCapture, format="bgr")
 
-for img in cam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+key_points = get_blobs(rawCapture)
 
-    frame = img.array
-    key_points = get_blobs(frame)
-    cv2.imshow('Captura', key_points)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
-    k = cv2.waitKey(1) & 0xff
-    if k == ESC:
-        cam.close()
-        break
-
-cv2.destroyAllWindows()
-
-
-
-
-img_BGR = cv2.imread("red_blue.jpg")
-redMin = (10, 10, 100)
-redMax = (50, 50, 255)
-
-get_blobs(img_BGR, redMin, redMax)
+cv2.imshow('Captura', key_points)
+cv2.imwrite('./foto.png', rawCapture) 
