eda0b9da018c1d96d1a590cd4c3c6cf68025c0f6
AdrianMartin <756524@unizar.es>
2021-03-23 13:28:57 +0100

    Added comments to math.py file

--- functions/math.py
+++ functions/math.py
@@ -6,10 +6,17 @@ def sigmoid(x, alpha):
     Sigmoid function implemented as hyperbolic tangent of x*alpha, where x is the independent variable and alpha the curve's slope measure
     :param x: independent variable
     :param alpha: measure of the curve's slope, where a high value means a high slope, and a low one a low slope
-    :return: a value between -1 and 1 if asPositive is false, else 0,1, the hyperbolic tangent of x*alpha
+    :return: a value between -1 and 1, the hyperbolic tangent of x*alpha
     """
     return np.tanh(x * alpha)
 
 
 def logistic(x, alpha, beta):
+    """
+    Sigmoid function implemented as logistic function of -alpha * (x - beta), where x is the independent variable, alpha the curve's slope measure and beta an offset over the axis value
+    :param x: independent variable
+    :param alpha: measure of the curve's slope, where a high value means a high slope, and a low one a low slope
+    :param beta: offset over the axis value
+    :return: a value between 0 and 1, the logistic function of -alpha * (x - beta)
+    """
     return 1 / (1 + pow(np.e, -alpha * (x - beta)))
