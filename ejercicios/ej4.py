afee9aa72c0cbd1c6827e6d8c9ebcae009c9b307
Alberto Martínez <45785416+ErTisho@users.noreply.github.com>
2021-04-14 20:42:56 +0200

    now the ej4 has restrictions on the sensor(random)

--- ejercicios/ej4.py
+++ ejercicios/ej4.py
@@ -6,34 +6,38 @@ from functions.functions import norm_pi, hom, loc
 from functions.simubot import simubot
 
 
+def sensor(robot):
+    # Estimated sonar restrictions
+    d_max = 30
+    d_min = 5
+    angle_max = np.deg2rad(30)
+
+    # If the th has too deviation, we use the max angle
+    angle = min(angle_max, robot[2]) if robot[2] > 0 else max(-angle_max, robot[2])
+
+    d = robot[1] / np.cos(angle)
+
+    return min(d_max,(max(d_min, d)))
 
 
 # Parameters
-"""
-k1 = 0.03
-k2 = -0.4
-w_max = np.deg2rad(30)
-dc_lat = 5
-dc_front = 10 
-"""
-
 k1 = 0.015
-k2 = -0.15
-v = 1
+k2 = -0.20
 
-w_max = np.deg2rad(15)
-dc_lat = 10
-dc_front = 5
-x_map_size = 60
+v = 1
+w_max = np.deg2rad(10)
+dc_lat = 10 # Objetive parameter
+dc_front = 5 # To stop
+x_map_size = 50
+y_map_size = 20
 
 # robot initialization
-robot = [0, 12, np.deg2rad(30)]
-
+robot = [0, 13, np.deg2rad(30)]
 
 # graph initialization
 plt.ion()
 f, (ax1, ax2, ax3) = plt.subplots(3, 1, num='ej4')
-plt.plot([0, x_map_size, x_map_size],[0, 0, x_map_size / 3])
+plt.plot([0, x_map_size, x_map_size],[0, 0, y_map_size])
 plt.gca().set_aspect(True)
 plt.show(block=False)
 
@@ -41,13 +45,13 @@ plt.show(block=False)
 vs = []
 ws = []
 
-d = robot[1] / np.cos(robot[2]) # Distancia
+d = robot[1] / np.cos(robot[2]) # Distance
 while True:
-    # get relative position
 
-    # TODO: funcion sensor. Limitar sensor??
+
     d_old = d
-    d = robot[1] / np.cos(robot[2]) # Medida sensor
+    d = sensor(robot) # Medida sensor
+    #d = robot[1] / np.cos(robot[2])
     d_var = d - d_old
 
     # calculate angular velocity
@@ -55,6 +59,12 @@ while True:
 
     # limitate velocity
     w = min(w_max, w) if w > 0 else max(-w_max, w)
+
+    print(robot[1])
+    print((dc_lat - d))
+    print(d_var)
+    print(w)
+    print("")
 
     # plot velocities
     for data, next, label, ax in [(vs, v, 'v', ax1), (ws, w, 'w', ax2)]:
@@ -65,17 +75,15 @@ while True:
 
     # update robot
     dibrobot(robot, 'green', 0.01)
-    robot = simubot([v, w], np.array(robot), 1)
+    robot = simubot([v, w], np.array(robot), 0.75)
     dibrobot(robot, 'blue', 0.01)
 
     # refresh map
     plt.gcf().canvas.draw()
     plt.gcf().canvas.flush_events()
 
-    print(robot[1])
-
     # check goal
-    if robot[0] > x_map_size - dc_front:
+    if robot[0] >= x_map_size - dc_front:
         break
 
 plt.show(block=True)
