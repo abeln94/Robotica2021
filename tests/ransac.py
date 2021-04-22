ccb2bba49e711e7af595ff02d78fc5fc6274c0c9
abeln94 <544125@unizar.es>
2021-04-22 11:34:23 +0200

    sample ransac

--- tests/ransac.py
+++ tests/ransac.py
@@ -1,7 +1,11 @@
-import cv2
 import time
 
+import cv2
+
+from classes import Cfg
 from classes.Robot import Robot
+
+Cfg.add_argument("-bb8", "--bb8", help="detect bb8", action="store_true")
 
 robot = None
 
@@ -14,14 +18,26 @@ if __name__ == "__main__":
         robot = Robot()
 
         # 1. 
-        image = cv2.imread("images/R2-D2_s.png", cv2.IMREAD_COLOR)
+        image_R2D2 = cv2.imread("images/R2-D2_s.png", cv2.IMREAD_COLOR)
+        image_BB8 = cv2.imread("images/BB8_s.png", cv2.IMREAD_COLOR)
+
+        our, other = (image_R2D2, image_BB8) if not Cfg.bb8 else (image_BB8, image_R2D2)
 
         # 2. 
         print("Press ESC to quit")
         while cv2.waitKey(1) != 27:
-            found, coordinates = robot.detectImage(image)
-            print("Returned (", found, ", ", coordinates, ")")
-            time.sleep(0.5)
+            foundOur, coordinatesOur = robot.detectImage(our)
+            foundOther, coordinatesOther = robot.detectImage(other)
+
+            if foundOur and foundOther:
+                if coordinatesOur[0] > coordinatesOther[0]:  # the camera is inverted
+                    print('Our is at the left')
+                else:
+                    print('Our is at the right')
+            else:
+                print('not found', foundOur, foundOther)
+
+            time.sleep(1)
 
     finally:
         # wrap up and close stuff before exiting
