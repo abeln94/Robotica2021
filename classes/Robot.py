93fffa2db20f2a7080a6791d6f6c24f7d390af16
tisho <albertisho99@gmail.com>
2021-04-22 10:04:59 +0200

    Add guro default value

--- classes/Robot.py
+++ classes/Robot.py
@@ -215,7 +215,7 @@ class Robot:
                 th = norm_pi(th + dth)
 
             # update ang with gyro
-            GYRO_DEFAULT = 0 # TODO
+            GYRO_DEFAULT = 2372 # TODO
             GYRO2DEG = 0 # TODO: O.25 ?
 
             gyro_data = self.BP.get_sensor(self.SENSOR_GYRO)[0]
@@ -223,10 +223,6 @@ class Robot:
             time_save = time.time()
             print("--------------- GYRO DATA: " + str(gyro_data))
             print("--------------- GYRO DATA CENTERED: " + str(gyro_data - GYRO_DEFAULT))
-
-            n += 1
-            gyro_data_mean = gyro_data_mean * (n-1) / n + gyro_data / n
-            print("--------------- GYRO DATA MEAN: " + str(gyro_data_mean))
 
             gyro_speed = (gyro_data - GYRO_DEFAULT) * GYRO2DEG
             # print("--------------- GYRO SPEED (rad/seg): " + str(gyro_speed))
