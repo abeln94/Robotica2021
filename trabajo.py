4ae5805e6ea13e9956c495b6472842fabb94c9c5
abeln94 <544125@unizar.es>
2021-04-22 12:55:51 +0200

    completed trabajo.py

--- trabajo.py
+++ trabajo.py
@@ -1,16 +1,22 @@
-import random
-import time
-
+import cv2
 import matplotlib
 import numpy as np
 
 from classes import Cfg
 from classes.Map import GRID
 from classes.MapLib import Map2D
+from classes.Periodic import Periodic
 from classes.Robot import Robot
 from p4 import traverseLabyrinth
 
 matplotlib.use("TkAgg")  # sudo apt-get install tcl-dev tk-dev python-tk python3-tk if TkAgg is not available
+
+Cfg.add_argument("-bb8", "--bb8", help="detect bb8", action="store_true")
+
+# images
+IMAGE_R2D2 = cv2.flip(cv2.imread("images/R2-D2_s.png", cv2.IMREAD_COLOR), -1)
+IMAGE_BB8 = cv2.flip(cv2.imread("images/BB8_s.png", cv2.IMREAD_COLOR), -1)
+IMAGE_OUR, IMAGE_OTHER = (IMAGE_R2D2, IMAGE_BB8) if not Cfg.bb8 else (IMAGE_BB8, IMAGE_R2D2)
 
 robot = None
 
@@ -32,7 +38,7 @@ if __name__ == "__main__":
         leftSide = robot.getLight() >= 0.5
 
         # perform S
-        T = 5
+        T = 10
         for side, c in ((1 if leftSide else -1, 6), (-1 if leftSide else 1, 4)):
             cx, cy = myMap._cell2pos(1, c)
             for t in range(T):
@@ -55,14 +61,20 @@ if __name__ == "__main__":
         robot.go(*myMap._cell2pos(1, 3.5))
 
         # look for ball
-        # robot.trackObject()
+        robot.trackObject()
 
         # position looking at the images
         robot.go(*myMap._cell2pos(0.5, 6))
         robot.lookAt(*myMap._cell2pos(0.5, 7))
 
         # detect image
-        leftExit = random.random() < 0.5  # TODO: robot detect image position
+        periodic = Periodic(1)
+        while periodic():
+            foundOur, coordinatesOur = robot.detectImage(IMAGE_OUR)
+            foundOther, coordinatesOther = robot.detectImage(IMAGE_OTHER)
+            if foundOur and foundOther:
+                leftExit = coordinatesOur[0] > coordinatesOther[0]  # the camera is inverted
+                break
 
         # exit lab
         if leftExit:
