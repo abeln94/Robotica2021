620b4cbb001c88880004bff96dfed07aec5b04a4
tisho <albertisho99@gmail.com>
2021-05-01 20:53:06 +0200

    cositas

--- functions/get_color_blobs.py
+++ functions/get_color_blobs.py
@@ -135,4 +135,5 @@ def position_reached(img_BGR, rangeMin=(160, 80, 50), rangeMax=(10, 255, 255)):
     mask = mask1 | mask2
 
     PX = int(Cfg.CAMERA_HEIGHT / 4.8)
+    print(cv2.countNonZero(mask[0:PX, :]) / Cfg.CAMERA_WIDTH / PX)
     return cv2.countNonZero(mask[0:PX, :]) / Cfg.CAMERA_WIDTH / PX > 0.6
